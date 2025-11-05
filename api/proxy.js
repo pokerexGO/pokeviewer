633199d4de126e5f47d9dd889af8ccbb25a9e7ea
pokerexGO <aeprojectdeveloper@gmail.com>
2025-11-05 05:14:23 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,4 @@
 // api/proxy.js
-import fetch from "node-fetch";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).json({ error: "Método no permitido" });
